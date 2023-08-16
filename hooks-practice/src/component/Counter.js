b8365b64cddc57199a8c9374b324d5dde03c0417
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2023-08-16 22:34:35 +0900

    #useState 여러번 사용하기

--- hooks-practice/src/component/Counter.js
+++ hooks-practice/src/component/Counter.js
@@ -2,12 +2,19 @@ import { useState } from 'react';
 
 const Counter = () => {
     const [value, setValue] = useState(0);
+    const [increment, setIncrement] = useState(0);
+
+    const onChangeIncrement = e => {
+        setIncrement(e.target.value);
+    }
+
     return (<div>
         <p>
             현재 카운터 값은 <b>{value}</b> 입니다.
         </p>
-        <button onClick={() => setValue(value + 1)}>+1</button>
-        <button onClick={() => setValue(value - 1)}>-1</button>
+        <input value={increment} onChange={onChangeIncrement}></input>
+        <button onClick={() => setValue(Number(value) + Number(increment))}>+{increment}</button>
+        <button onClick={() => setValue(Number(value) - Number(increment))}>-{increment}</button>
     </div>);
 };
 
